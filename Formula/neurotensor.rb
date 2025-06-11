5c7b53fa5db86ddd95dbb446bcbae0508866c5fb
moldenha <smoldenha@gmail.com>
2025-06-11 11:06:51 -0400

    Updated shah for v0.1.5

--- Formula/neurotensor.rb
+++ Formula/neurotensor.rb
@@ -9,7 +9,7 @@ class Neurotensor < Formula
     sha256 "30dead6cf600cf19f7ca619d44afb452df50251dfded66b33b7d99e9292254d8"
   elsif Hardware::CPU.intel?
     url "https://github.com/moldenha/NeuroTensor/releases/download/v0.1.5/neurotensor-macos-x86_64-v0.1.5.tar.gz"
-    sha256 "d8569e679256db5528cac639f2ad7b49360f25e507b7e5f5e8d6b0374a63a02c"
+    sha256 "71af4b520defcab39c7bacff91d6bedf3569b12105bea6154aef313672ad9710"
   end
   
 
