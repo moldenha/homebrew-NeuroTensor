6d205a3a429b0f0fa55fd90ae1b05eba960b2a8d
moldenha <smoldenha@gmail.com>
2025-06-11 03:39:02 -0400

    Updated shah for v0.1.5

--- Formula/neurotensor.rb
+++ Formula/neurotensor.rb
@@ -9,22 +9,15 @@ class Neurotensor < Formula
     sha256 "30dead6cf600cf19f7ca619d44afb452df50251dfded66b33b7d99e9292254d8"
   elsif Hardware::CPU.intel?
     url "https://github.com/moldenha/NeuroTensor/releases/download/v0.1.5/neurotensor-macos-x86_64-v0.1.5.tar.gz"
-    sha256 "e36c56fe7987c741f97f2c7018307564c66df6e99b79cfc25316b610c672215f"
+    sha256 "d8569e679256db5528cac639f2ad7b49360f25e507b7e5f5e8d6b0374a63a02c"
   end
   
-  resource "simde" do
-    url "https://github.com/simd-everywhere/simde/archive/refs/tags/v0.8.2.tar.gz"
-    sha256 "ed2a3268658f2f2a9b5367628a85ccd4cf9516460ed8604eed369653d49b25fb"
-  end
 
   def install
     # This installs libneurotensor.a into Homebrew’s lib path
     lib.install "lib/libneurotensor.a"
     include.install Dir["include/*"] if File.directory?("include")
 
-    resource("simde").stage do
-      include.install "simde"
-    end
   end
 
 
